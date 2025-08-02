3ed737b0ddf72d1c32bcbd32d41e59f770ac0f81
HasanSCULPT <hyasuf8@gmail.com>
2025-08-02 14:57:17 +0300

    Update Lung Cancer_app.py

--- Lung Cancer_app.py
+++ Lung Cancer_app.py
@@ -210,7 +210,7 @@ with col2:
             "ANXIETY": [int(anxiety)],
             "ALCOHOL CONSUMING": [int(alcohol)],
             "COUGHING": [int(cough)],
-            "SHORTNESS OF BREATH": [int(breath)],
+            "SHORT_BREATH": [int(breath)],
             "CHEST PAIN": [int(chest_pain)],
             "SYMPTOM_SCORE": [symptom_score],
             "LIFESTYLE_SCORE": [lifestyle_score]
